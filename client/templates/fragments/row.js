215650b9a35d08d56e3742de922c1055db28d9af
Nicholas Valbusa <squallstar@gmail.com>
2015-11-17 11:52:36 +0000

    row status

--- client/templates/fragments/row.js
+++ client/templates/fragments/row.js
@@ -1,5 +1,13 @@
 Template.fragmentsListRow.helpers({
   user: function () {
     return Users.findOne(this.user);
+  },
+  status: function () {
+    if (this.fetched_at) {
+      return 'success';
+    }
+  }
+});
+
   }
 });
