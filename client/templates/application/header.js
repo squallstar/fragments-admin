0a3d6a3dd6515787583d49a2d78962aa0773f0b0
Nicholas Valbusa <squallstar@gmail.com>
2015-11-17 12:01:09 +0000

    refactor on header routes

--- client/templates/application/header.js
+++ client/templates/application/header.js
@@ -1,5 +1,11 @@
 Template.header.helpers({
   currentRoute: function () {
     return Router.current().route.getName();
+  },
+  navRoutes: function () {
+    return [
+      { name: 'Users', route: 'users.list' },
+      { name: 'Fragments', route: 'fragments.list' }
+    ];
   }
 });
